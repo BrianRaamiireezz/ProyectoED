5364a1d4c05aee707ae7b64a26652a5a27fcac91
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2022-11-22 19:56:01 -0600

    Terminada version con un SUS

--- Main.py
+++ Main.py
@@ -1,1 +1,41 @@
 # Check https://pinout.xyz/pinout/pin12_gpio18
+
+# Core
+import RPi.GPIO as GPIO
+import time
+
+# Clases
+from SensorUS import SensorUS
+from RFID import RFID
+
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# Sensor
+sensor = SensorUS(18, 24)
+sensor.setup()
+
+# RFID
+lector = RFID()
+
+# Ciclo
+try:
+
+  while True:
+
+    # Obtener distancia
+    distancia = sensor.getDistance()
+
+    # Si la distancia es igual o menor a 5 cm
+    if distancia <= 5.0:
+
+      # Leer tarjeta
+      lector.read()
+
+    # Delay
+    time.sleep(1)
+
+except KeyboardInterrupt:
+
+  print('Ejecucion detenida por el usuario')
+  GPIO.cleanup()
--- RFID.py
+++ RFID.py
@@ -27,4 +27,32 @@ class RFID:
 
     return stringUID
 
+  def read(self):
+
+    leyendo = True
+
+    while leyendo:
+
+      # Escanear
+      (status, TagType) = self.__reader.MFRC522_Request(self.__reader.PICC_REQIDL)
+
+      # Si se detecta tarjeta
+      if status == self.__reader.MI_OK:
+
+        # Obtener UID de la tarjeta
+        (status, uid) = self.__reader.MFRC522_SelectTagSN()
+
+        # Si se tiene el UID, continuar
+        if status == self.__reader.MI_OK:
+
+          print("UID de la tarjeta: %s" % self.uidToString(uid))
+
+          # Aqui se desbloquearía la puerta y se checa lo de las camaras
+
+          # Salir de lectura
+          leyendo = False
+
+        else:
+          print("Error de autenticacion")
+
   # Check - Solo es un objeto para las dos RFID
